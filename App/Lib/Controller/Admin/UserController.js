6e0a964d39d359aa396c6e5cad8f09ac650137fc
wangwenbo <wangwenbo000@me.com>
2015-06-10 18:01:45 +0800

    vue遍历列表尝试

--- App/Lib/Controller/Admin/UserController.js
+++ App/Lib/Controller/Admin/UserController.js
@@ -17,7 +17,19 @@ module.exports = Controller("Admin/BaseController", function(){
             var getPage = self.get("page");
             getUserList.getList(getPage).then(function (data) {
                 var pager = new Pager(data, baseUrl);
-                self.assign({'userList':data.data,'pager':pager.render(),'userCount':data.count});
+                self.assign({'userList':data.data,'pager':pager.render(
+                    '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
+                    '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
+                    "3",
+                    "...",
+                    {
+                        itemTag: 'li', // 每个按钮的标签
+                        textTag: 'span', // 分割符文本的标签
+                        currentClass: 'active', // 当前选中的页码 Class
+                        prevClass: 'prev', // 上一页 Class
+                        nextClass: 'next' // 下一页 Class
+                    }
+                ),'userCount':data.count});
                 return self.display();
             })
         }
