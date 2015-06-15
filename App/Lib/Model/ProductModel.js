5f5a3b327f12f8222392682f9e9d3781e8bde022
wangwenbo <wangwenbo000@me.com>
2015-06-15 23:54:45 +0800

    一些调整

--- App/Lib/Model/ProductModel.js
+++ App/Lib/Model/ProductModel.js
@@ -3,7 +3,6 @@ module.exports = Model(function(){
         //获取用户列表
         getProduct: function(getPage){
             return D('Products').order('id DESC').select().then(function(data){
-                console.log(data);
                 return data;
             });
         },
