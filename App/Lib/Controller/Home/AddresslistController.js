ba724ee963a1b31da0540b5387ff552eb7f559ca
wangwenbo <wangwenbo000@me.com>
2015-06-27 14:20:29 +0800

    前台页面增加

---
+++ App/Lib/Controller/Home/AddresslistController.js
@@ -0,0 +1,17 @@
+/**
+ * controller
+ * @return
+ */
+module.exports = Controller("Home/BaseController", function(){
+    "use strict";
+    return {
+        indexAction: function(){
+            //render View/Home/index_index.html file
+            this.display();
+        },
+        _404Action: function(){
+            this.status(404); //发送404状态码
+            this.end('404 not found');
+        }
+    };
+});
