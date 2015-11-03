bce48af389e5bb919d274fdcfae15fcd374a8174
wangwenbo <wangwenbo.89@gmail.com>
2015-11-04 02:12:44 +0800

    修改首页的部分功能

--- App/Conf/config.js
+++ App/Conf/config.js
@@ -3,10 +3,10 @@ module.exports = {
   port: 1288, //监听的端口
   db_type: 'mysql', // 数据库类型
   db_host: 'localhost', // 服务器地址
-  db_port: '', // 端口
+  db_port: '3307', // 端口
   db_name: 'wechat_shopping', // 数据库名
   db_user: 'root', // 用户名
-  db_pwd: '', // 密码
+  db_pwd: '15117909848', // 密码
   db_prefix: 'king_', // 数据库表前缀
 
   'app_group_list': ['Home', 'Admin'],
