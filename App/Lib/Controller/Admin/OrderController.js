992d3ce1cce9fae62f30de6c169ed03ef53336f9
wangwenbo <wangwenbo000@me.com>
2015-11-05 18:16:49 +0800

    尝试一个方法实现

--- App/Lib/Controller/Admin/OrderController.js
+++ App/Lib/Controller/Admin/OrderController.js
@@ -5,98 +5,133 @@
  * controller
  * @return
  */
-module.exports = Controller("Admin/BaseController", function(){
-    "use strict";
-    var  Q = require('q');
-    return {
-        indexAction: Q.async(function* (){
-            var rp = require('request-promise');
-            var baseUrl = "/Admin/order/index.html";
-            var orderModel = D('Order');
-            var moment = require('moment');
-            var nextDay = moment().add(1,'days').format('YYYY-MM-DD');
-            // var getPage = this.get('page')||0;
-            var data = yield orderModel.getorderlist(this.get('page'));
-            // console.log('a',this.get('page'));
+module.exports=Controller("Admin/BaseController", function(){
+  "use strict";
+  var Q=require('q');
+  return {
+    indexAction: Q.async(function* (){
+      var rp=require('request-promise');
+      var baseUrl="/Admin/order/index.html";
+      var orderModel=D('Order');
+      var moment=require('moment');
+      var nextDay=moment().add(1, 'days').format('YYYY-MM-DD');
+      // var getPage = this.get('page')||0;
+      var data=yield orderModel.getorderlist(this.get('page'));
+      // console.log('a',this.get('page'));
 
-            var expressData = yield D('Express').order('ID DESC').field(['name','id']).select();
-            var ExpressSelectData = [];
-            for(var k in expressData){
-              var ExpressSelectJson = {};
-              ExpressSelectJson['text'] = expressData[k].name;
-              ExpressSelectJson['value'] = expressData[k].id;
-              ExpressSelectData.push(ExpressSelectJson);
-            }
-            var expressAddress = yield orderModel.expressAddress();
-            this.assign({'listJSON':data.data,'total':data.total,'count':data.count,'express':ExpressSelectData,'expressAddress':expressAddress,"nextday":nextDay});
-            this.display();
-        }),
-        allocationAction:Q.async(function* (){
-          var getUpdateId = JSON.parse(this.post('updateId'));
-          var expresserid = parseInt(this.post('data'));
-          var data = yield D('Orderproductcopy').where({id:['IN',getUpdateId]}).update({expresserid:expresserid});
-          return this.success({info:data});
-        }),
-        filterAction:function(){
-            var self = this;
-            var getJSON = self.post('fliterjson');
-            var getPage = self.post('pagenum');
-            var orderModel = D('order');
-            orderModel.orderfilter(JSON.parse(getJSON),parseInt(getPage)).then(function(data){
-                return self.end(data);
-            });
-        },
-        getsingleorderinfoAction:function(){
-            var self =this;
-            var orderid = self.post('id');
-            var orderModel = D('order').getorderinfobyid(orderid).then(function(data){
-                return self.end(data);
-            })
-        },
-        getattachmentinfoAction:function(){
-            var self = this;
-            var getPointId = self.post('pointid');
-            var getSelectId = self.post('id');
-            var orderModel = D('order').getattachmentinfo(getSelectId,getPointId).then(function(data){
-                return self.end(data);
-            })
-        },
-        getpayinfoAction:Q.async(function*(){
-          var getOrderId = parseInt(this.post('id'));
-          var getPayInfoById = yield D('payinfo').where({'attach':getOrderId}).select();
-          formatTime(getPayInfoById, 'llll', 'time_end');
-          return this.end(getPayInfoById);
-        }),
-        changeOrderStatePayAction:Q.async(function* (){
-          var getId = this.post('id');
-          yield D('Orderproductcopy').where({orderid:getId}).update({
-            productstate:'30'
-          });
-          yield D('Order').where({id:getId}).update({
-            paystate:'1',
-          });
-          this.success();
-        }),
-        changeOrderStateCompleteAction:Q.async(function* (){
-          var getId = this.post('id');
-          yield D('Orderproductcopy').where({orderid:getId}).update({
-            productstate:'55'
-          });
-          yield D('Order').where({id:getId}).update({
-            paystate:'1',
-          });
-          yield D('Order').where({id: getId}).updateDec('nowstate');
-          this.success();
-        }),
-        changeOrderStateCancelAction:Q.async(function* (){
-          var getId = this.post('id');
-          yield D('Orderproductcopy').where({orderid:getId}).update({
-            productstate:'60'
-          });
-          yield D('Order').where({id:getId}).update({
-            nowstate:'0'
-          });
-          this.success();
-        }),
-    };
+      var expressData=yield D('Express').order('ID DESC').field(['name', 'id']).select();
+      var ExpressSelectData=[];
+      for(var k in expressData){
+        var ExpressSelectJson={};
+        ExpressSelectJson['text']=expressData[k].name;
+        ExpressSelectJson['value']=expressData[k].id;
+        ExpressSelectData.push(ExpressSelectJson);
+      }
+      var expressAddress=yield orderModel.expressAddress();
+      this.assign({
+        'listJSON': data.data,
+        'total': data.total,
+        'count': data.count,
+        'express': ExpressSelectData,
+        'expressAddress': expressAddress,
+        "nextday": nextDay
+      });
+      this.display();
+    }),
+    allocationAction: Q.async(function* (){
+      var getUpdateId=JSON.parse(this.post('updateId'));
+      var expresserid=parseInt(this.post('data'));
+      var data=yield D('Orderproductcopy').where({id: ['IN', getUpdateId]}).update({expresserid: expresserid});
+      return this.success({info: data});
+    }),
+    filterAction: function(){
+      var self=this;
+      var getJSON=self.post('fliterjson');
+      var getPage=self.post('pagenum');
+      var orderModel=D('order');
+      orderModel.orderfilter(JSON.parse(getJSON), parseInt(getPage)).then(function(data){
+        return self.end(data);
+      });
+    },
+    getsingleorderinfoAction: function(){
+      var self=this;
+      var orderid=self.post('id');
+      var orderModel=D('order').getorderinfobyid(orderid).then(function(data){
+        return self.end(data);
+      })
+    },
+    getattachmentinfoAction: function(){
+      var self=this;
+      var getPointId=self.post('pointid');
+      var getSelectId=self.post('id');
+      var orderModel=D('order').getattachmentinfo(getSelectId, getPointId).then(function(data){
+        return self.end(data);
+      })
+    },
+    getpayinfoAction: Q.async(function*(){
+      var getOrderId=parseInt(this.post('id'));
+      var getPayInfoById=yield D('payinfo').where({'attach': getOrderId}).select();
+      formatTime(getPayInfoById, 'llll', 'time_end');
+      return this.end(getPayInfoById);
+    }),
+    changeOrderStatePayAction: Q.async(function* (){
+      var getId=this.post('id');
+      yield D('Orderproductcopy').where({orderid: getId}).update({
+        productstate: '30'
+      });
+      yield D('Order').where({id: getId}).update({
+        paystate: '1',
+      });
+      this.success();
+    }),
+    changeOrderStateCompleteAction: Q.async(function* (){
+      var getId=this.post('id');
+      yield D('Orderproductcopy').where({orderid: getId}).update({
+        productstate: '55'
+      });
+      yield D('Order').where({id: getId}).update({
+        paystate: '1',
+      });
+      yield D('Order').where({id: getId}).updateDec('nowstate');
+      this.success();
+    }),
+    changeOrderStateCancelAction: Q.async(function* (){
+      var getId=this.post('id');
+      yield D('Orderproductcopy').where({orderid: getId}).update({
+        productstate: '60'
+      });
+      yield D('Order').where({id: getId}).update({
+        nowstate: '0'
+      });
+      this.success();
+    }),
+
+    /**
+     * 获取订单列表
+     * 获取全部     get_orderList()
+     * 获取条件     get_orderList({where:{}})
+     */
+    get_orderListAction: Q.async(function*(){
+      if(this.post()){
+        var oConfigJson=this.post();
+        var orderListResult=yield D('Order').get_orderList(oConfigJson);
+        return this.success(orderListResult);
+      }else{
+        return this.error();
+      }
+    }),
+
+    /**
+     * 获取配送地点
+     * 获取全部     get_orderList()
+     * 获取条件     get_orderList({where:{}})
+     */
+    get_expressLocaleAction: Q.async(function*(){
+      if(this.post()){
+
+      }else{
+        return this.error();
+      }
+    })
+  };
 });
--- App/Lib/Model/OrderModel.js
+++ App/Lib/Model/OrderModel.js
@@ -1,100 +1,115 @@
-module.exports = Model(function() {
-  var Q = require("q");
-  var moment = require('moment');
-  var nextDay = moment().add(1,'days').format('YYYY-MM-DD');
+module.exports=Model(function(){
+  var Q=require("q");
+  var moment=require('moment');
+  var nextDay=moment().add(1, 'days').format('YYYY-MM-DD');
   return {
     //获取用户列表
-    getorderlist: function(getPage) {
-      return D('Orderproductcopy').where({'time': nextDay}).order('id DESC').page(getPage, 20).countSelect().then(function(data) {
+    getorderlist: function(getPage){
+      return D('Orderproductcopy').where({'time': nextDay}).order('id DESC').page(getPage, 20).countSelect().then(function(data){
         staticFilter(data.data);
-        formatTime(data.data,'YYYY-MM-DD dddd','time');
+        formatTime(data.data, 'YYYY-MM-DD dddd', 'time');
         return data;
       });
     },
     expressAddress: Q.async(function* (){
-      return yield D('Orderproductcopy').where({'time':nextDay}).distinct('addressKey').select();
+      return yield D('Orderproductcopy').where({'time': nextDay}).distinct('addressKey').select();
     }),
-    getstatecount: function(data) {
+    getstatecount: function(data){
       return D('Orderproductcopy').where({
         'productstate': ['=', data]
-      }).countSelect().then(function(data) {
+      }).countSelect().then(function(data){
         return data;
       });
     },
-    getorderinfobyid: function(id) {
+    getorderinfobyid: function(id){
       return D('Orderproductcopy').where({
         'id': id
-      }).select().then(function(data) {
+      }).select().then(function(data){
         staticFilter(data);
         return data;
       })
     },
-    getattachmentinfo: function(orderid, pointid) {
-      var attachmentJSON = {};
+    getattachmentinfo: function(orderid, pointid){
+      var attachmentJSON={};
       return D('Order').where({
         'id': orderid
-      }).select().then(function(data) {
-        for (var k in data) {
-          switch (data[k].orderfrom) {
+      }).select().then(function(data){
+        for(var k in data){
+          switch(data[k].orderfrom){
             case 2:
-              data[k].orderfrom = "PC用户";
-              data[k].orderstate = "warning";
+              data[k].orderfrom="PC用户";
+              data[k].orderstate="warning";
               break;
             case 1:
-              data[k].orderfrom = "微信用户";
-              data[k].orderstate = "success";
+              data[k].orderfrom="微信用户";
+              data[k].orderstate="success";
               break;
             default:
-              data[k].orderfrom = "未知终端";
-              data[k].orderstate = "danger";
+              data[k].orderfrom="未知终端";
+              data[k].orderstate="danger";
           }
         }
         formatTime(data, 'llll', 'ordertime');
-        attachmentJSON.attachment = data[0];
+        attachmentJSON.attachment=data[0];
         return {
           'json': data,
           'now': pointid
         };
-      }).then(function(data) {
-        var pointId = parseInt(data.now);
-        var userId = data.json[0].userid;
-        var Ptotal = 0
+      }).then(function(data){
+        var pointId=parseInt(data.now);
+        var userId=data.json[0].userid;
+        var Ptotal=0
         return D('Orderproductcopy').where({
           'orderid': data.json[0].id
-        }).select().then(function(data) {
+        }).select().then(function(data){
           staticFilter(data);
-          for (var k in data) {
-            Ptotal += data[k].productnum;
-            switch (data[k].id) {
+          for(var k in data){
+            Ptotal+=data[k].productnum;
+            switch(data[k].id){
               case pointId:
-                data[k].thisPoint = "warning";
+                data[k].thisPoint="warning";
                 break;
               default:
-                data[k].thisPoint = 'a';
+                data[k].thisPoint='a';
             }
           }
           formatTime(data, 'YY-MM-DD dddd', 'time');
-          attachmentJSON.attach = data;
-          attachmentJSON.attachment.countDays = data.length;
-          attachmentJSON.attachment.productTotal = Ptotal;
+          attachmentJSON.attach=data;
+          attachmentJSON.attachment.countDays=data.length;
+          attachmentJSON.attachment.productTotal=Ptotal;
           return userId;
         });
-      }).then(function(userid) {
+      }).then(function(userid){
         return D('Users').where({
           'id': userid
-        }).select().then(function(data) {
-          attachmentJSON.userInfo = data[0];
+        }).select().then(function(data){
+          attachmentJSON.userInfo=data[0];
           return attachmentJSON;
         });
       })
     },
-    orderfilter: function(json, getPage) {
-      return D('Orderproductcopy').where(json).order('id DESC').page(getPage, 20).countSelect().then(function(data) {
+    orderfilter: function(json, getPage){
+      return D('Orderproductcopy').where(json).order('id DESC').page(getPage, 20).countSelect().then(function(data){
         // console.log(data);
         staticFilter(data.data);
-        formatTime(data.data,'YYYY-MM-DD dddd','time');
+        formatTime(data.data, 'YYYY-MM-DD dddd', 'time');
         return data;
       });
-    }
+    },
+    /**
+     * 获取订单列表
+     * 获取全部     get_orderList()
+     * 获取条件     get_orderList({where:{}})
+     */
+    get_orderList: Q.async(function*(oConfigJson){
+      if(isObject(oConfigJson)){
+        if(isEmpty(oConfigJson)){
+          return yield D('Order').order('id DESC').page().countSelect();
+        }else{
+          return yield D('Order').where(oConfigJson.where).order('id DESC').page().countSelect();
+        }
+      }
+    }),
+
   }
 })
